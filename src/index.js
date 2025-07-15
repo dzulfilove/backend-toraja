21980afe2c82268791a05fe390c21fc47c552263
dzulfivec <dzulfivector@gmail.com>
2025-07-15 23:02:04 +0700

    dkdkdkd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const functions = require("firebase-functions");
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
